6f16b609bf8bb339dbf5918436cfa215259235c2
ClMacher <clmacher@gmail.com>
2024-03-20 01:37:34 -0300

    Creación del componente modal y adición de Bootstrap

---
+++ src/components/googleEarth/modalEnviarPoligono.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function PoligonoInfoModal({ showModal, closeModal, enviarPoligono }) {
+  return (
+    <div className={`modal ${showModal ? 'show' : ''}`} id="poligonoInfoModal" tabIndex="-1" aria-labelledby="poligonoInfoModal" aria-hidden={!showModal}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">Ingresar información</h5>
+            <button type="button" className="btn-close" onClick={closeModal} aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="project-name" className="col-form-label">Nombre del proyecto:</label>
+                <input type="text" className="form-control" id="project-name" required />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="col-form-label">E-mail:</label>
+                <input type="text" className="form-control" id="email" required />
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" onClick={closeModal} data-bs-dismiss="modal">Cancelar</button>
+            <button type="button" id="btnEnviarPoligono" className="btn btn-primary" onClick={enviarPoligono}>Enviar polígono</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PoligonoInfoModal;
