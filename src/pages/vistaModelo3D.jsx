27c50040851bcdb944d15630b08c8dabaa2f702a
ClMacher <clmacher@gmail.com>
2024-03-27 14:44:32 -0300

    Import de font Quicksand

--- src/pages/vistaModelo3D.jsx
+++ src/pages/vistaModelo3D.jsx
@@ -46,7 +46,7 @@ function GoogleEarthComponent() {
       viewer.generateCombineGltf();
     };
 
-  
+
     ui.onTileSliderChange = (value) => {
       for (let i = 0; i < viewer.gltfArray.length; i++) {
         const gltf = viewer.gltfArray[i];
@@ -190,7 +190,7 @@ function GoogleEarthComponent() {
   //   viewer.computarModeloGltf();
   // };
 
-  function computarModeloGltf(){
+  function computarModeloGltf() {
     viewer.computarModeloGltf();
   }
 
@@ -208,12 +208,17 @@ function GoogleEarthComponent() {
         href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossOrigin=""
+
       />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet"></link>
       <script
         src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
         integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
         crossOrigin=""
       ></script>
+
 
       <div id="settings">
         <div id="logoGrav">
