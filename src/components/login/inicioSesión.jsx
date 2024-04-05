b6387b104e28bd92e919a5fb44274ee787ec5b3e
DominiqueDVD <dominique.delvalle3d@gmail.com>
2024-04-05 01:22:27 -0300

    Cambio de dependencias

--- src/components/login/inicioSesión.jsx
+++ src/components/login/inicioSesión.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import GoogleLogin from "react-google-login";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import { googleLogout } from "@react-oauth/google";
 import "../../styles/inicioSesión.css";
 
 function InicioSesión() {
@@ -7,6 +9,10 @@ function InicioSesión() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogout = () => {
+    googleLogout();
+  };
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -23,12 +29,6 @@ function InicioSesión() {
   const handleEmailLogin = () => {
     // Aquí puedes agregar la lógica para iniciar sesión con email y contraseña
     console.log("Iniciar sesión con email:", email, "Contraseña:", password);
-    setIsLoggedIn(true);
-  };
-
-  const handleGoogleLogin = (response) => {
-    console.log(response);
-    // Aquí puedes realizar cualquier lógica adicional que necesites para el inicio de sesión con Google
     setIsLoggedIn(true);
   };
 
@@ -58,25 +58,40 @@ function InicioSesión() {
             onChange={handlePasswordChange}
           />
           <i
-            className={`fa ${showPassword ? "fa-eye-slash" : "fa-eye"} password-icon`}
+            className={`fa ${
+              showPassword ? "fa-eye-slash" : "fa-eye"
+            } password-icon`}
             onClick={handleTogglePasswordVisibility}
           />
         </div>
-        <br /><br />
-        <button className="loginButton" onClick={handleEmailLogin}>Acceder</button>
-        <br /><br />
+        <br />
+        <br />
+        <button className="loginButton" onClick={handleEmailLogin}>
+          Acceder
+        </button>
+        <br />
+        <br />
         <div className="botonGoogle">
-          <GoogleLogin
-            clientId="876703133488-3391b4edgncgv88rvpttbfdob8qc45b2.apps.googleusercontent.com"
-            buttonText="Continuar con Google"
-            onSuccess={handleGoogleLogin}
-            onFailure={handleGoogleLogin}
-            cookiePolicy={"single_host_origin"}
-          />
+          <GoogleOAuthProvider clientId="876703133488-3391b4edgncgv88rvpttbfdob8qc45b2.apps.googleusercontent.com">
+            <GoogleLogin
+              onSuccess={(credentialResponse) => {
+                console.log(credentialResponse);
+              }}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+              useOneTap
+            />
+          
+          </GoogleOAuthProvider>
+          {/*<button onClick={handleLogout}>Logout</button>*/}
+         
         </div>
         <br />
         <br />
-        <p>¿No tienes una cuenta? <a href="#">Regístrate aquí</a>.</p>
+        <p>
+          ¿No tienes una cuenta? <a href="#">Regístrate aquí</a>.
+        </p>
       </div>
     </div>
   );
